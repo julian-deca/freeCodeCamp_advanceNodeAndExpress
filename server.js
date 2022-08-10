989e82a60f85dce8f9c27eff5b18fe0c34ee438f
julian <juliandecastelli@gmail.com>
2022-08-10 17:04:34 -0300

    completed test 12

--- server.js
+++ server.js
@@ -1,21 +1,131 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const session = require("express-session");
+const passport = require("passport");
 const app = express();
-
+const ObjectID = require("mongodb").ObjectID;
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcrypt");
 fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "pug");
 
-app.route('/').get((req, res) => {
+app.use(passport.initialize());
+app.use(
+  passport.session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log("User " + username + " attempted to log in.");
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+  app.route("/").get((req, res) => {
+    res.render("pug", {
+      title: "Connected to Database",
+      message: "Please login",
+      showLogin: true,
+      showRegistration: true,
+    });
+  });
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  app.route("/register").post(
+    (req, res, next) => {
+      const hash = bcrypt.hashSync(req.body.password, 12);
+      myDataBase.findOne({ username: req.body.username }, function (err, user) {
+        if (err) {
+          next(err);
+        } else if (user) {
+          res.redirect("/");
+        } else {
+          myDataBase.insertOne(
+            {
+              username: req.body.username,
+              password: hash,
+            },
+            (err, doc) => {
+              if (err) {
+                res.redirect("/");
+              } else {
+                // The inserted document is held within
+                // the ops property of the doc
+                next(null, doc.ops[0]);
+              }
+            }
+          );
+        }
+      });
+    },
+    passport.authenticate("local", { failureRedirect: "/" }),
+    (req, res, next) => {
+      res.redirect("/profile");
+    }
+  );
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("pug", {
+      title: e,
+      message: "Unable to login",
+    });
+  });
+});
+app.post(
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/profile");
+  }
+);
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/");
+}
+app.get("/profile", ensureAuthenticated, (req, res) => {
+  res.render(process.cwd() + "/views/pug/profile", {
+    username: req.user.username,
+  });
+});
+app.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
